fd1eb69e9f70d58ed6264b3ada20134bce5b668e
Ste517 <grakulino07@gmail.com>
2025-10-26 02:10:11 +0200

    Added GUI

--- funzionilimoncello.h
+++ funzionilimoncello.h
@@ -1,93 +1,95 @@
-#ifndef CALCOLATORELIMONCELLO_FUNZIONILIMONCELLO_NEW
-#define CALCOLATORELIMONCELLO_FUNZIONILIMONCELLO_NEW
+#ifndef FUNZIONILIMONCELLO_H
+#define FUNZIONILIMONCELLO_H
 
-#endif //CALCOLATORELIMONCELLO_FUNZIONILIMONCELLO_NEW
+#endif // FUNZIONILIMONCELLO_H
 
 #include <iostream>
 
 // Costanti fisiche globali
 namespace Costanti {
-    const double DENSITA_ALCOOL = 0.789;   // g/ml per alcol etilico 96%
-    const double GRADO_ALCOOL = 0.96;      // 96% vol
-    const double PERCENTUALE_ACQUA_ALCOOL = 1 - GRADO_ALCOOL;
-    const double ACQUA_BUCCE = 0.70;       // 70% acqua nelle bucce
-    const double CONTRAZIONE = 0.03;       // contrazione volumetrica alcol-acqua 3%
-    const double CONTRIBUTO_ZUCCHERO = 0.625; // ml/g volume aumentato da zucchero
+const double DENSITA_ALCOOL = 0.789;   // g/ml per alcol etilico 96%
+const double GRADO_ALCOOL = 0.96;      // 96% vol
+const double PERCENTUALE_ACQUA_ALCOOL = 1 - GRADO_ALCOOL;
+const double ACQUA_BUCCE = 0.70;       // 70% acqua nelle bucce
+const double CONTRAZIONE = 0.03;       // contrazione volumetrica alcol-acqua 3%
+const double CONTRIBUTO_ZUCCHERO = 0.625; // ml/g volume aumentato da zucchero
 }
 
 class Tentativi {
-    public:
-        double massaAlcool;
-        double massaZucchero;
-        double massaAcqua;
-        double gradazione;
-        double dolcezza;
+public:
+    double massaAlcool;
+    double massaZucchero;
+    double massaAcqua;
+    double gradazione;
+    double dolcezza;
 
-        Tentativi(int mAlcool, int mZucchero, int mAcqua) {
-            // Crea un tentativo in base a dati già esistenti
-            massaAlcool = mAlcool;
-            massaZucchero = mZucchero;
-            massaAcqua = mAcqua;
-            massaBucce = mAlcool / 2.0;
-            acquaBucce = massaBucce * Costanti::ACQUA_BUCCE;
-            massaAlcoolPuro = massaAlcool * Costanti::GRADO_ALCOOL;
-            volumeAlcol = massaAlcoolPuro / Costanti::DENSITA_ALCOOL;
-            volumeZucchero = massaZucchero * Costanti::CONTRIBUTO_ZUCCHERO;
-            massaAcquaTotale = massaAcqua + acquaBucce + (massaAlcool - massaAlcoolPuro);
-            volumeAcqua = massaAcquaTotale;
-            volumeTotale = (volumeAlcol + volumeAcqua) * (1.0- Costanti::CONTRAZIONE);
-            gradazione = (volumeAlcol / volumeTotale) * 100.0;
-            dolcezza = massaZucchero / volumeTotale;
-        }
+    Tentativi(int mAlcool, int mZucchero, int mAcqua) {
+        // Crea un tentativo in base a dati già esistenti
+        massaAlcool = mAlcool;
+        massaZucchero = mZucchero;
+        massaAcqua = mAcqua;
+        massaBucce = mAlcool / 2.0;
+        acquaBucce = massaBucce * Costanti::ACQUA_BUCCE;
+        massaAlcoolPuro = massaAlcool * Costanti::GRADO_ALCOOL;
+        volumeAlcol = massaAlcoolPuro / Costanti::DENSITA_ALCOOL;
+        volumeZucchero = massaZucchero * Costanti::CONTRIBUTO_ZUCCHERO;
+        massaAcquaTotale = massaAcqua + acquaBucce + (massaAlcool - massaAlcoolPuro);
+        volumeAcqua = massaAcquaTotale;
+        volumeTotale = (volumeAlcol + volumeAcqua) * (1.0- Costanti::CONTRAZIONE);
+        gradazione = (volumeAlcol / volumeTotale) * 100.0;
+        dolcezza = massaZucchero / volumeTotale;
+    }
 
-        Tentativi(Tentativi &t, double massaBucceLimone) {
-            // Crea un tentativo a partire da un altro tentativo, sapendo la quantità di bucce di limone che si hanno
-            massaAlcool = massaBucceLimone * 2;
-            massaBucce = massaBucceLimone;
-            massaZucchero = (massaAlcool * t.massaZucchero) / t.massaAlcool;
-            massaAcqua = (massaAlcool * t.massaAcqua) / t.massaAlcool;
-            acquaBucce = massaBucce * Costanti::ACQUA_BUCCE;
-            massaAlcoolPuro = massaAlcool * Costanti::GRADO_ALCOOL;
-            volumeAlcol = massaAlcoolPuro / Costanti::DENSITA_ALCOOL;
-            volumeZucchero = massaZucchero * Costanti::CONTRIBUTO_ZUCCHERO;
-            massaAcquaTotale = massaAcqua + acquaBucce + (massaAlcool - massaAlcoolPuro);
-            volumeAcqua = massaAcquaTotale;
-            volumeTotale = (volumeAlcol + volumeAcqua) * (1.0- Costanti::CONTRAZIONE);
-            gradazione = (volumeAlcol / volumeTotale) * 100.0;
-            dolcezza = massaZucchero / volumeTotale;
-        }
+    Tentativi(Tentativi &t, double massaBucceLimone) {
+        // Crea un tentativo a partire da un altro tentativo, sapendo la quantità di bucce di limone che si hanno
+        massaAlcool = massaBucceLimone * 2;
+        massaBucce = massaBucceLimone;
+        massaZucchero = (massaAlcool * t.massaZucchero) / t.massaAlcool;
+        massaAcqua = (massaAlcool * t.massaAcqua) / t.massaAlcool;
+        acquaBucce = massaBucce * Costanti::ACQUA_BUCCE;
+        massaAlcoolPuro = massaAlcool * Costanti::GRADO_ALCOOL;
+        volumeAlcol = massaAlcoolPuro / Costanti::DENSITA_ALCOOL;
+        volumeZucchero = massaZucchero * Costanti::CONTRIBUTO_ZUCCHERO;
+        massaAcquaTotale = massaAcqua + acquaBucce + (massaAlcool - massaAlcoolPuro);
+        volumeAcqua = massaAcquaTotale;
+        volumeTotale = (volumeAlcol + volumeAcqua) * (1.0- Costanti::CONTRAZIONE);
+        gradazione = (volumeAlcol / volumeTotale) * 100.0;
+        dolcezza = massaZucchero / volumeTotale;
+    }
 
-        Tentativi(double massaBucceLimone, double gradazioneDesiderata, double dolcezzaDesiderata) {
-            // Crea un tentativo a partire dalla gradazione e la dolcezza desiderata, sapendo la quantità di bucce di limone che si hanno
-            massaAlcool = massaBucceLimone * 2;
-            massaBucce = massaBucceLimone;
-            massaAlcoolPuro = massaAlcool * Costanti::GRADO_ALCOOL;
-            volumeAlcol = massaAlcoolPuro / Costanti::DENSITA_ALCOOL;
-            gradazione = gradazioneDesiderata;
-            massaZucchero = (volumeAlcol*dolcezzaDesiderata*100) / gradazioneDesiderata;
-            acquaBucce = massaBucce * Costanti::ACQUA_BUCCE;
-            volumeZucchero = massaZucchero * Costanti::CONTRIBUTO_ZUCCHERO;
-            volumeTotale = (massaZucchero/dolcezzaDesiderata);
-            volumeAcqua = (volumeTotale/(1- Costanti::CONTRAZIONE))-volumeAlcol;
-            massaAcquaTotale = volumeAcqua;
-            massaAcqua = volumeAcqua - (acquaBucce + (massaAlcool - massaAlcoolPuro));
-            dolcezza = dolcezzaDesiderata;
-        }
+    Tentativi(double massaBucceLimone, double gradazioneDesiderata, double dolcezzaDesiderata) {
+        // Crea un tentativo a partire dalla gradazione e la dolcezza desiderata, sapendo la quantità di bucce di limone che si hanno
+        massaAlcool = massaBucceLimone * 2;
+        massaBucce = massaBucceLimone;
+        massaAlcoolPuro = massaAlcool * Costanti::GRADO_ALCOOL;
+        volumeAlcol = massaAlcoolPuro / Costanti::DENSITA_ALCOOL;
+        gradazione = gradazioneDesiderata;
+        massaZucchero = (volumeAlcol*dolcezzaDesiderata*100) / gradazioneDesiderata;
+        acquaBucce = massaBucce * Costanti::ACQUA_BUCCE;
+        volumeZucchero = massaZucchero * Costanti::CONTRIBUTO_ZUCCHERO;
+        volumeTotale = (massaZucchero/dolcezzaDesiderata);
+        volumeAcqua = (volumeTotale/(1- Costanti::CONTRAZIONE))-volumeAlcol;
+        massaAcquaTotale = volumeAcqua;
+        massaAcqua = volumeAcqua - (acquaBucce + (massaAlcool - massaAlcoolPuro));
+        dolcezza = dolcezzaDesiderata;
+    }
 
-        void displayValori() const {
-            std::cout << "Massa alcool: " << massaAlcool << std::endl;
-            std::cout << "Massa zucchero: " << massaZucchero << std::endl;
-            std::cout << "Massa acqua: " << massaAcqua << std::endl;
-            std::cout << "Gradazione: " << gradazione << std::endl;
-            std::cout << "Dolcezza: " << dolcezza << std::endl;
-        }
-    private:
-        double massaBucce;
-        double acquaBucce;
-        double massaAlcoolPuro;
-        double massaAcquaTotale;
-        double volumeAlcol;
-        double volumeAcqua;
-        double volumeZucchero;
-        double volumeTotale;
+    void displayValori() const {
+        std::cout << "Massa alcool: " << massaAlcool << std::endl;
+        std::cout << "Massa zucchero: " << massaZucchero << std::endl;
+        std::cout << "Massa acqua: " << massaAcqua << std::endl;
+        std::cout << "Gradazione: " << gradazione << std::endl;
+        std::cout << "Dolcezza: " << dolcezza << std::endl;
+    }
+private:
+    double massaBucce;
+    double acquaBucce;
+    double massaAlcoolPuro;
+    double massaAcquaTotale;
+    double volumeAlcol;
+    double volumeAcqua;
+    double volumeZucchero;
+    double volumeTotale;
 };
+
+Tentativi primotentativo(95,100,130);
---
+++ mainwindow.cpp
@@ -0,0 +1,111 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QWidget>
+#include "funzionilimoncello.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    switch (ui->sceltaCalcolo->currentIndex()) {
+        case 0:
+            ui->tentativoObiettivo->hide();
+            ui->tentativoDati->hide();
+            break;
+        case 1:
+            ui->tentativoPrecedente->hide();
+            ui->tentativoDati->hide();
+            break;
+        case 2:
+            ui->tentativoObiettivo->hide();
+            ui->tentativoPrecedente->hide();
+            break;
+        default:
+            ui->tentativoObiettivo->hide();
+            ui->tentativoPrecedente->hide();
+            ui->tentativoDati->hide();
+            break;
+    }
+    ui->risultati->hide();
+    ui->risultati_2->hide();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_sceltaCalcolo_currentIndexChanged(int index)
+{
+    switch (index) {
+    case 0:
+        ui->tentativoPrecedente->show();
+        ui->tentativoObiettivo->hide();
+        ui->tentativoDati->hide();
+        break;
+    case 1:
+        ui->tentativoObiettivo->show();
+        ui->tentativoPrecedente->hide();
+        ui->tentativoDati->hide();
+        break;
+    case 2:
+        ui->tentativoDati->show();
+        ui->tentativoObiettivo->hide();
+        ui->tentativoPrecedente->hide();
+        break;
+    default:
+        ui->tentativoObiettivo->hide();
+        ui->tentativoPrecedente->hide();
+        ui->tentativoDati->hide();
+        break;
+    }
+    ui->risultati->hide();
+    ui->risultati_2->hide();
+    ui->labelGradazioneAlcolica->clear();
+    ui->labelDolcezza_2->clear();
+    ui->grammiacqua->clear();
+    ui->grammialcol->clear();
+    ui->grammizucchero->clear();
+}
+
+void MainWindow::on_calcolaDaDati_clicked()
+{
+    Tentativi tentativo(ui->massaAlcool->value(),ui->massaZucchero->value(),ui->massaAcqua->value()); //alcol,zucchero,acqua
+    ui->risultati->show();
+    ui->labelDolcezza_2->setNum(tentativo.dolcezza);
+    ui->labelGradazioneAlcolica->setNum(tentativo.gradazione);
+}
+
+
+void MainWindow::on_calcolaDaObiettivo_clicked()
+{
+    Tentativi tentativo(ui->massaScorze->value(),ui->gradazione->value(),ui->dolcezza->value());
+    ui->risultati->show();
+    ui->risultati_2->show();
+    ui->labelDolcezza_2->setNum(tentativo.dolcezza);
+    ui->labelGradazioneAlcolica->setNum(tentativo.gradazione);
+    ui->grammiacqua->setNum(tentativo.massaAcqua);
+    ui->grammialcol->setNum(tentativo.massaAlcool);
+    ui->grammizucchero->setNum(tentativo.massaZucchero);
+}
+
+
+void MainWindow::on_calcoloDaTentativo_clicked()
+{
+    Tentativi tentativo(0,0,0);
+    double buccelimone = ui->massaScorze1->value();
+    switch (ui->sceltaTentativo->currentIndex()) {
+        default:
+            Tentativi t(primotentativo,buccelimone);
+            tentativo = t;
+    }
+    ui->risultati->show();
+    ui->risultati_2->show();
+    ui->labelDolcezza_2->setNum(tentativo.dolcezza);
+    ui->labelGradazioneAlcolica->setNum(tentativo.gradazione);
+    ui->grammiacqua->setNum(tentativo.massaAcqua);
+    ui->grammialcol->setNum(tentativo.massaAlcool);
+    ui->grammizucchero->setNum(tentativo.massaZucchero);
+}
+
